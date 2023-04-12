a3ea27dce4dd2180ff42fdf0001f09a7559c62ea
amyxichardson <c0036225@my.shu.ac.uk>
2023-04-12 14:51:29 +0000

    Basic HTML added, nav-bar created and views paths configured

--- reviewApp/application/views.py
+++ reviewApp/application/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'application/home.html', {'title': 'Home'})
+
+def about(request):
+    return render(request, 'application/about.html')
+
+def contact(request):
+    return render(request, 'application/contact.html')
+
+def product(request):
+    return render(request, 'application/product.html')
