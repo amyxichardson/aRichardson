59b7b33aaf191147cd9c0f5abda06d1e138a440b
amyxichardson <c0036225@my.shu.ac.uk>
2023-04-14 22:36:07 +0000

    User can create an account and register

--- reviewApp/reviewApp/settings.py
+++ reviewApp/reviewApp/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'application',
+    'users',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
