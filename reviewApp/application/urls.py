a4fc676e1169bd3d64b95f6f96771a006d379bf4
amyxichardson <c0036225@my.shu.ac.uk>
2023-04-19 12:17:04 +0000

    Reviews show on product pages

--- reviewApp/application/urls.py
+++ reviewApp/application/urls.py
@@ -7,4 +7,7 @@ urlpatterns =[
     path('contact', views.contact, name='application-contact'),
     path('product', views.product, name='application-product'),
     path('product_list', views.all_products, name='list-product'),
+    path('show_product/<product_id>', views.show_product, name="show-product"),
+    path('show_review/<review_id>', views.show_review, name="show-review"),
+
 ]
--- reviewApp/application/views.py
+++ reviewApp/application/views.py
@@ -14,7 +14,28 @@ def contact(request):
 def product(request):
     return render(request, 'application/product.html', {'title': 'Product'})
 
+
 def all_products(request):
     product_list = Product.objects.all()
+   
     return render(request, 'application/product_list.html',
     {'product_list': product_list})
+
+
+def show_product(request, product_id):
+    product=Product.objects.get(pk=product_id)
+    review=product.review_set.all()
+    
+    return render(request, 'application/show_product.html',
+    {'product':product,
+    'review':review})
+
+
+def show_review(request, review_id):
+    review=Review.objects.get(pk=review_id)
+    
+    return render(request, 'application/show_review.html',
+    {'review':review})
+
+
+
